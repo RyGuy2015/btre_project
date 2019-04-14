fdae1a16021f0db9c3f00caba225f8d7517376ee
Ryan Ingram <ryani@dpslink.com>
2019-04-14 13:08:49 -0400

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ryaningram/Development/misc/python/btre_project/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
