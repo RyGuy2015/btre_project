1bc57c7725095bba9b4c80bd3f04a0b013fee64c
Ryan Ingram <ryani@dpslink.com>
2019-05-13 21:07:49 -0400

    modified local_settings

--- btre/settings.py
+++ btre/settings.py
@@ -143,13 +143,6 @@ MESSAGE_TAGS = {
 
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ry.ingram01@gmail.com'
-EMAIL_HOST_PASSWORD = 'Shayla07'
-EMAIL_USE_TLS=True
-
 
 try:
     from .local_settings import *
